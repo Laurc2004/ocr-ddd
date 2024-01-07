c14ceea85b22088b3b06a3f4f44e5f9aacda9d49
刘睿超 <LiuRC2004@outlook.com>
2024-01-07 21:00:20 +0800

    fix:解决Swagger不支持文件上传

--- ocr-trigger/src/main/java/com/lrc/ocr/http/OcrController.java
+++ ocr-trigger/src/main/java/com/lrc/ocr/http/OcrController.java
@@ -3,10 +3,7 @@ package com.lrc.ocr.http;
 import com.lrc.ocr.domain.model.aggregate.ApiDataAggregate;
 import com.lrc.ocr.domain.service.IOcrService;
 import com.lrc.ocr.model.Result;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
@@ -25,7 +22,7 @@ public class OcrController {
      * @return
      */
     @PostMapping("/getText")
-    public Result<List<String>> getTextOnlyByFile(MultipartFile file){
+    public Result<List<String>> getTextOnlyByFile(@RequestPart("file") MultipartFile file){
         List<String> text = ocrService.getText(file);
         return Result.success(text);
     }
@@ -36,7 +33,7 @@ public class OcrController {
      * @return
      */
     @PostMapping("/getToal")
-    public Result<List<ApiDataAggregate>> getTotalByFile(MultipartFile file){
+    public Result<List<ApiDataAggregate>> getTotalByFile(@RequestPart("file") MultipartFile file){
         List<ApiDataAggregate> apiDataAggregate = ocrService.getToal(file);
         return Result.success(apiDataAggregate);
     }
