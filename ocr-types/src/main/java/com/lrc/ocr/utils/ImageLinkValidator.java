c9fc8c748750fa13c3a41d0c7bf0f80b63c51f31
刘睿超 <LiuRC2004@outlook.com>
2024-01-10 21:28:06 +0800

    fix:图片链接工具类

--- ocr-types/src/main/java/com/lrc/ocr/utils/ImageLinkValidator.java
+++ ocr-types/src/main/java/com/lrc/ocr/utils/ImageLinkValidator.java
@@ -1,14 +1,16 @@
 package com.lrc.ocr.utils;
 
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import lombok.extern.slf4j.Slf4j;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Slf4j
 public class ImageLinkValidator {
-    private static final String regex = ".*\\.(jpg|jpeg|png|gif|bmp)$";
-    private static final Pattern pattern = Pattern.compile(regex);
+
     /**
      * 判断给定的URL是否是图片链接
      *
@@ -18,13 +20,26 @@ public class ImageLinkValidator {
 
     public static boolean isImageLink(String img) {
         try {
+            // 新建url和连接
             URL url = new URL(img);
-            URLConnection uc = url.openConnection();
-            InputStream in = uc.getInputStream();
-            if (img.equalsIgnoreCase(uc.getURL().toString()))
-                in.close();
-            return true;
-        } catch (Exception e) {
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            // 设置连接超时时间为3秒
+            connection.setConnectTimeout(3000);
+            // 设置读取超时时间为3秒
+            connection.setReadTimeout(3000);
+            // 立刻尝试连接，以启动计时
+            connection.connect();
+            // 检查是否成功连接
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                // 读取图像
+                BufferedImage image = ImageIO.read(connection.getInputStream());
+                log.info("解析图片:{}",image);
+                return image != null;
+            }
+            // 如果响应码不是HTTP_OK，则认为不是图片链接
+            return false;
+        } catch (IOException e) {
+            // 在读取图片时发生异常，可能不是图片链接
             return false;
         }
     }
